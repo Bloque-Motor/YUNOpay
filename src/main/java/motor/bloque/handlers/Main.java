a728d753a82d75308ce0aa740aaae98d479d3883
Pablo Beltrán de Casso <beltrancasso@hotmail.com>
2018-12-29 19:29:00 +0100

    Main class implemented
    Main method calls for data to be retrieved from the persistence file and initiates the user interface. When the user selects the option to exit the program, data is written to persistence file

--- src/main/java/motor/bloque/handlers/Main.java
+++ src/main/java/motor/bloque/handlers/Main.java
@@ -1,9 +1,28 @@
 package motor.bloque.handlers;
 
-//TODO: This class will load the whole program at startup.
+//TODO: Replace Terminal with GUI
+
+import motor.bloque.gui.Terminal;
+import motor.bloque.interfaces.Card;
+import motor.bloque.interfaces.Movement;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class Main {
-    public static void main(String[] args){
 
+    private static List cards;
+    private static List movements;
+
+    public static void main(String[] args){
+        cards = Persistence.getCards();
+        if (cards == null) cards = new ArrayList<Card>();
+        movements = Persistence.getMovements();
+        if (movements == null) movements = new ArrayList<Movement>();
+
+        //Provisional
+        Terminal.mainMenu();
+        if (!cards.isEmpty()) Persistence.saveCards(cards);
+        if (!movements.isEmpty()) Persistence.saveMovements(movements);
     }
 }
--- src/main/java/motor/bloque/handlers/Persistence.java
+++ src/main/java/motor/bloque/handlers/Persistence.java
@@ -3,5 +3,32 @@ package motor.bloque.handlers;
 //TODO: this class will handle file read and write operations
 //I personally suggest writing JSON objects as plain text for persistence
 
+import motor.bloque.interfaces.Card;
+import motor.bloque.interfaces.Movement;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
 public class Persistence {
+
+    private Map<Integer,Card> cards;
+    private ArrayList<Movement> movements;
+
+    public Persistence(){}
+
+
+    public static List getCards() {
+        return null;
+    }
+
+    public static List getMovements() {
+        return null;
+    }
+
+    public static void saveCards(List cards) {
+    }
+
+    public static void saveMovements(List movements) {
+    }
 }
