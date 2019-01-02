07c1e37d378e5e4621710c094672d2c5ec1f76f0
Pablo Beltrán de Casso <beltrancasso@hotmail.com>
2019-01-02 22:21:40 +0100

    Main menu window and corresponding button event listeners

--- src/main/java/motor/bloque/handlers/ClientController.java
+++ src/main/java/motor/bloque/handlers/ClientController.java
@@ -1,8 +1,62 @@
 package motor.bloque.handlers;
 
-import motor.bloque.interfaces.ClientModel;
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 //TODO: This class must handle all operations for the elements on the gui.
 
-public class ClientController implements ClientModel {
+public abstract class ClientController extends AbstractAction {
+
+    public static class NewCardButton implements ActionListener{
+        @Override
+        public void actionPerformed(ActionEvent e){
+            System.out.println("New card button pressed");
+        }
+    }
+
+    public static class PayButton implements ActionListener {
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            System.out.println("Pay button pressed");
+        }
+    }
+
+    public static class RechargeMoneyButton implements ActionListener {
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            System.out.println("Recharge money button pressed");
+        }
+    }
+
+    public static class ChangePinButton implements ActionListener {
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            System.out.println("Change PIN button pressed");
+        }
+    }
+
+    public static class ConsultBalanceButton implements ActionListener {
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            System.out.println("Consult balance button pressed");
+        }
+    }
+
+    public static class ConsultMovementsButton implements ActionListener {
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            System.out.println("Consult movements button pressed");
+        }
+    }
+
+    public static class QuitButton implements ActionListener {
+
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            System.out.println("Quit button pressed");
+            Persistence.saveAll();
+            System.exit(0);
+        }
+    }
 }
