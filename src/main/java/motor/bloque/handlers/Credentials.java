c4f52deb24b8fc06fd66f199e65438e04ad610f9
Pablo Beltrán de Casso <beltrancasso@hotmail.com>
2019-01-02 13:50:53 +0100

    Bug fix

--- src/main/java/motor/bloque/handlers/Credentials.java
+++ src/main/java/motor/bloque/handlers/Credentials.java
@@ -34,7 +34,7 @@ public class Credentials {
         Card card = Persistence.getCard(cardNumber);
         String salt = card.getSalt();
 
-        String toValidate = getSecurePassword(password, salt.getBytes());
+        String toValidate = getSecurePassword(password, hexStringToByteArray(salt));
         if (toValidate != null) return toValidate.equals(card.getHashedPIN());
         return false;
     }
@@ -63,6 +63,16 @@ public class Credentials {
             hexChars[j * 2 + 1] = hexArray[v & 0x0F];
         }
         return new String(hexChars);
+    }
+
+    private static byte[] hexStringToByteArray(String s) {
+        int len = s.length();
+        byte[] data = new byte[len / 2];
+        for (int i = 0; i < len; i += 2) {
+            data[i / 2] = (byte) ((Character.digit(s.charAt(i), 16) << 4)
+                    + Character.digit(s.charAt(i+1), 16));
+        }
+        return data;
     }
 
     private static String getSecurePassword(String passwordToHash, byte[] salt)
