34ad04170a614f69f3b3ecfadf5c0b2d69b9fe14
Pablo Beltrán de Casso <beltrancasso@hotmail.com>
2019-01-03 22:28:08 +0100

    New card and pay windows created
    No functionality attached to them

--- src/main/java/motor/bloque/handlers/ClientController.java
+++ src/main/java/motor/bloque/handlers/ClientController.java
@@ -1,17 +1,49 @@
 package motor.bloque.handlers;
 
+import motor.bloque.gui.ClientView;
+
 import javax.swing.*;
+import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
 
 //TODO: This class must handle all operations for the elements on the gui.
 
 public abstract class ClientController extends AbstractAction {
 
+    private static ClientView clientView;
+
+    public static void startClientApp() {
+
+        EventQueue.invokeLater(() -> {
+            ClientView frame = new ClientView();
+            ClientController.setFrame(frame);
+            frame.addWindowListener(new WindowAdapter() {
+                @Override
+                public void windowClosing(WindowEvent e) {
+                    //if(JOptionPane.showConfirmDialog(frame, "Are you sure ?") == JOptionPane.OK_OPTION){
+                    frame.setVisible(false);
+                    new ClientController.QuitButton().actionPerformed(null);
+                    frame.dispose();
+                    //}
+                }
+            });
+            frame.setVisible(true);
+        });
+    }
+
+    public static void setFrame(ClientView frame){
+        clientView = frame;
+    }
+
     public static class NewCardButton implements ActionListener{
         @Override
         public void actionPerformed(ActionEvent e){
             System.out.println("New card button pressed");
+            EventQueue.invokeLater(() ->clientView.changePanel(ClientView.panels.NEWCARD));
+
         }
     }
 
@@ -19,6 +51,7 @@ public abstract class ClientController extends AbstractAction {
         @Override
         public void actionPerformed(ActionEvent e) {
             System.out.println("Pay button pressed");
+            EventQueue.invokeLater(() ->clientView.changePanel(ClientView.panels.PAY));
         }
     }
 
