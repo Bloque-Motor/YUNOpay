d170df2238481d8f12dba2560f99998db093b008
Pablo Beltrán de Casso <beltrancasso@hotmail.com>
2019-01-03 22:28:08 +0100

    New card and pay windows created
    No functionality attached to them

--- src/main/java/motor/bloque/handlers/YUNO.java
+++ src/main/java/motor/bloque/handlers/YUNO.java
@@ -3,12 +3,11 @@ package motor.bloque.handlers;
 //TODO: Replace Terminal with GUI
 
 import motor.bloque.gui.ClientView;
-import motor.bloque.gui.Terminal;
 
 public class YUNO {
 
     public static void main(String[] args){
         Persistence.loadPersistence();
-        ClientView.startClientApp();
+        ClientController.startClientApp();
     }
 }
