66039ec2254e73d69386a907bd52859b44f5d2df
Pablo Beltrán de Casso <beltrancasso@hotmail.com>
2019-01-04 17:27:37 +0100

    Reorganized packages

--- src/main/java/motor/bloque/handlers/YUNO.java
+++ src/main/java/motor/bloque/handlers/YUNO.java
@@ -2,12 +2,12 @@ package motor.bloque.handlers;
 
 //TODO: Replace Terminal with GUI
 
-import motor.bloque.gui.ClientView;
+import motor.bloque.controllers.MainMenuController;
 
 public class YUNO {
 
     public static void main(String[] args){
         Persistence.loadPersistence();
-        ClientController.startClientApp();
+        MainMenuController.startClientApp();
     }
 }
