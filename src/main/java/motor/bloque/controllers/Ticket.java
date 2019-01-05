d130216cdf8a940ace31d19f2c77a62528f8da8f
Pablo Beltrán de Casso <beltrancasso@hotmail.com>
2019-01-05 19:18:07 +0100

    Consult balance controller finished

--- src/main/java/motor/bloque/controllers/Ticket.java
+++ src/main/java/motor/bloque/controllers/Ticket.java
@@ -8,6 +8,7 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 public abstract class Ticket extends AbstractAction {
+
     public static class OkButton implements ActionListener {
         @Override
         public void actionPerformed(ActionEvent e) {
@@ -15,4 +16,14 @@ public abstract class Ticket extends AbstractAction {
             EventQueue.invokeLater(() ->MainMenu.getFrame().changePanel(ClientView.panels.MAIN));
         }
     }
+
+    public static void balanceMessage(String cardNumber, int balance, String name){
+        String ticket = "Dear " + name + "<br><br>Card Number: ";
+        String hiddenNumber = "XXXX XXXX " + cardNumber.substring(7);
+        ticket = ticket + hiddenNumber + "<br>" + "Balance: " + balance;
+        MainMenu.getFrame().setTicket(ticket);
+        EventQueue.invokeLater(() ->MainMenu.getFrame().changePanel(ClientView.panels.TICKET));
+    }
+
+
 }
