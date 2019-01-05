254d1dd85d2cf5183069790761d06cf3ea5e4f41
Pablo Beltrán de Casso <beltrancasso@hotmail.com>
2019-01-05 20:10:13 +0100

    Performance improvements

--- src/main/java/motor/bloque/controllers/Ticket.java
+++ src/main/java/motor/bloque/controllers/Ticket.java
@@ -21,23 +21,23 @@ public abstract class Ticket extends AbstractAction {
     }
 
     public static void balanceMessage(String cardNumber, int balance, String name){
-        String ticket = "Dear " + name + "<br><br>Card Number: ";
+        StringBuilder ticket = new StringBuilder("Dear " + name + "<br><br>Card Number: ");
         String hiddenNumber = "XXXX XXXX " + cardNumber.substring(7);
-        ticket = ticket + hiddenNumber + "<br>" + "Balance: " + balance;
-        MainMenu.getFrame().setTicket(ticket);
+        ticket.append(hiddenNumber + "<br>" + "Balance: " + balance);
+        MainMenu.getFrame().setTicket(ticket.toString());
         EventQueue.invokeLater(() ->MainMenu.getFrame().changePanel(ClientView.panels.TICKET));
     }
 
     public static void movementsMessage(String cardNumber, List<Movement> movements, String name) {
-        String ticket = "Dear " + name + "<br><br>Card Number: ";
+        StringBuilder ticket = new StringBuilder("Dear " + name + "<br><br>Card Number: ");
         String hiddenNumber = "XXXX XXXX " + cardNumber.substring(7);
-        ticket = ticket + hiddenNumber + "<br><br>";
+        ticket.append(hiddenNumber + "<br><br>");
         for(Movement move : movements){
             LocalDateTime date = move.getDate();
-            ticket = ticket + date.getDayOfMonth() + "/" + date.getMonthValue() + "/" + date.getYear()%100;
-            ticket = ticket + " " + move.getAmount() + "<br>";
+            ticket.append(date.getDayOfMonth() + "/" + date.getMonthValue() + "/" + date.getYear()%100);
+            ticket.append(" " + move.getAmount() + "<br>");
         }
-        MainMenu.getFrame().setTicket(ticket);
+        MainMenu.getFrame().setTicket(ticket.toString());
         EventQueue.invokeLater(() ->MainMenu.getFrame().changePanel(ClientView.panels.TICKET));
     }
 
